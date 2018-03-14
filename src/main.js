15850095e975c22fb673d62bb8885b0aad041da8
DW <285677359@qq.com>
2018-03-14 19:41:30 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import 'babel-polyfill' /* 解析一些es6方法 */
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick' /* 解决移动端点击三毫秒的延迟 */
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
